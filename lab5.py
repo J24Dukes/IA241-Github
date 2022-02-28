2730888a425a038dc168473016197a69ab4812f7
EC2 Default User <ec2-user@ip-172-31-40-216.ec2.internal>
2022-02-28 21:26:46 +0000

    Lec5

---
+++ lab5.py
@@ -0,0 +1,58 @@
+'''
+lab5
+'''
+
+#3.1
+alien_color = 'green'
+
+if alien_color == 'green':
+    print('You have earned 5 points')
+    
+#3.2
+alien_color = 'red'
+
+if alien_color == 'green':
+    print('You have earned 5 points')
+
+else: 
+    print('You have earned 10 points')
+    
+#3.3
+
+favorite_fruits = ['Apple','Bannana','Orange']
+
+if 'mango' in favorite_fruits:
+    print('You really like mango')
+    
+if 'Orange' in favorite_fruits:
+    print('I eat orange')
+    
+if 'Tomatoe' in favorite_fruits:
+    print('Boo!')
+    
+#3.4
+if 5<10:
+    print('Kid')
+if 10<15<20:
+    print('Teenager')
+if 20<=25:
+    print("Adult")
+if 65<66:
+    print('Elder')
+    
+#3.4 Professors way
+age = 54 
+
+if age < 10:
+    print('Kid')
+
+elif age >= and age<20:
+    print('Teenager')
+    
+else:
+    print('Adult')
+    
+    if age>= 65:
+        print('Elder')
+        
+        
